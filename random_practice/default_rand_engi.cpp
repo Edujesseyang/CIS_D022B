c94df16f3ba510c8e881cbf475d408bfcf9ec03d
Edujesseyang <edujesseyang@gmail.com>
2024-04-20 19:30:41 -0700

    rand added

--- random_practice/default_rand_engi.cpp
+++ random_practice/default_rand_engi.cpp
@@ -1,16 +1,14 @@
 #include <random>
 #include <iostream>
 
-int main ()
+int main()
 {
-    std::random_device rd;
-    std::default_random_engine generator(rd());
-    std::uniform_int_distribution<int> distribution(0, 100);
+    std::random_device rd;                                   // random_device can get a real random num, use it as a seed for default random engine.
+    std::default_random_engine generator(rd());              // seed the default random engine.
+    std::uniform_int_distribution<int> distribution(0, 100); // define a distribution with type and bounds.
 
-    for (int i = 0; i < 100; i++)
-    {
-        std::cout << distribution(generator) << std::endl;
-    }
+    int a = distribution(generator);    // define an int and give it a rand val.
+    std::cout << a << '\n';
 
     return 0;
 }
