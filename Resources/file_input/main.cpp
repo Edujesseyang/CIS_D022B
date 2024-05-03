ea68be99875a46818295545232db7bb6790d7717
Edujesseyang <edujesseyang@gmail.com>
2024-05-03 14:38:31 -0700

    add

--- Resources/file_input/main.cpp
+++ Resources/file_input/main.cpp
@@ -4,11 +4,23 @@
 int main ()
 {
     int num = 0, sum = 0;
-    while(std::cin >> num) // use this syntax only if you want to get input from file. for input from user keyboard. DO NOT DO THIS. 
+    // while(std::cin >> num) // use this syntax only if you want to get input from file. for input from user keyboard. DO NOT DO THIS. 
+    // {
+    //     sum += num;    
+    // }
+
+    // anther way to do same thing.
+    // get a bool to check if there in more input.
+    std::istream& instream = std::cin >> num; 
+    bool more_input = static_cast<bool>(instream);
+
+    // if there is more, do the loop.
+    while(more_input)
     {
         sum += num;
-              
+        std::cin >> num;
     }
+
     std::cout << sum << '\n';
     return 0;
 }
